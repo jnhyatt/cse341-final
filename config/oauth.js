8bc332d5b8c7f4a2107eef0d24132152f5c79e69
JoshValjosh <joshua.n.hyatt@gmail.com>
2025-12-05 22:57:15 -0700

    Clean up auth stuff

--- config/oauth.js
+++ config/oauth.js
@@ -1,6 +1,7 @@
 import { db } from "./db.js";
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { createUser } from "../services/createUser.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -12,31 +13,13 @@ passport.use(
             clientSecret: process.env.OAUTH_CLIENT_SECRET,
             callbackURL: "/auth/callback",
         },
-        async function (accessToken, refreshToken, profile, done) {
-            // In a typical OAuth2 flow, we need to fetch the user profile
-            // using the access token. This depends on your OAuth provider.
+        async function (_accessToken, _refreshToken, profile, done) {
             try {
-                // Fetch user info from the OAuth provider
-                const response = await fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                });
-
-                if (!response.ok) {
-                    return done(new Error("Failed to fetch user info"));
-                }
-
                 let user = await db.collection("users").findOne({ _id: profile.id });
 
                 if (!user) {
-                    user = {
-                        _id: profile.id,
-                        handle: "placeholder_handle",
-                        bookmarkedPosts: [],
-                        following: [],
-                    };
-                    await db.collection("users").insertOne(user);
+                    await createUser(profile.displayName || "Player", profile.id);
+                    user = await db.collection("users").findOne({ _id: profile.id });
                 }
 
                 return done(null, user);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,5 @@
-import { db } from "../config/db.js";
-import { createUser } from "../services/createUser.js";
-
 export async function handleCallback(req, res) {
-    try {
-        // Check if user exists, create if not
-        const existingUser = await db.collection("users").findOne({ _id: req.user.id });
-        if (!existingUser) {
-            await createUser(req.user.displayName || "Player", req.user.id);
-        }
-        res.redirect("/me");
-    } catch (err) {
-        console.error("Error creating user:", err);
-        res.redirect("/me");
-    }
+    res.redirect("/me");
 }
 
 export function logout(req, res) {
